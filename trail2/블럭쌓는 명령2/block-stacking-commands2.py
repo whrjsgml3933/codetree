420bdb0819c0a3df41ae633e183504babd1784c1
조건희 <whrjsgml3933@gmail.com>
2026-09-18 08:55:41 +0900

    [Wrong Answer] 블럭쌓는 명령2 (49ms, 16MB)

--- trail2/블럭쌓는 명령2/block-stacking-commands2.py
+++ trail2/블럭쌓는 명령2/block-stacking-commands2.py
@@ -4,6 +4,6 @@ commands = [tuple(map(int, input().split())) for _ in range(k)]
 # Please write your code here.
 result = []
 for i in range(k):
-    result.append((n-k))
+    result.append(abs(n-k))
 
 print(max(result))
